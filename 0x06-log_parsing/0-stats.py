4bf09531a42e2a5238ee7415b618b705ade9bae1
ferchislopez0910 <ferchislopez0910@gmail.com>
2022-01-27 11:55:03 -0500

    Merge branch 'main' of https://github.com/ferchislopez0910/holbertonschool-interview into main

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -6,21 +6,20 @@ Task - Script that reads stdin line by line and computes metrics
 import sys
 
 
-if __name__ == "__main__":
-    statusCode = {"200": 0,
-               "301": 0,
-               "400": 0,
-               "401": 0,
-               "403": 0,
-               "404": 0,
-               "405": 0,
-               "500": 0}
+def parse_line(line):
+        """ Read, parse and grab data"""
+        if __name__ == "__main__":
+            statusCode = {"200": 0,
+                          "301": 0,
+                          "400": 0,
+                          "401": 0,
+                          "403": 0,
+                          "404": 0,
+                          "405": 0,
+                          "500": 0}
     count = 1
     file_size = 0
-
-    def parse_line(line):
-        """ Read, parse and grab data"""
-        try:
+       try:
             p_line = line.split()
             status_code = p_line[-2]
             if status_code in statusCode.keys():
